86bc9da66751364444f0b577ab390efa10a57222
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2026-02-17 22:30:20 +0000

    fix extract subtomo

--- src/himena_relion/relion5_tomo/widgets/_extract.py
+++ src/himena_relion/relion5_tomo/widgets/_extract.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+import os
 from pathlib import Path
 import logging
 import time
@@ -27,9 +29,7 @@ class QExtractJobViewer(QJobScrollArea):
 
         self._last_force_reloaded = -1.0
         self._num_page = 50
-        self._current_num_extracts = 0
         self._last_updated_dir: str = ""
-        self._subtomo_pattern = "{ith}_data.mrc"
         self._tomo_list = QMicrographListWidget(["Tomogram", "Subtomogram Path"])
         self._tomo_list.setFixedHeight(130)
         self._tomo_list.current_changed.connect(self._on_tomo_changed)
@@ -38,8 +38,9 @@ class QExtractJobViewer(QJobScrollArea):
         self._slider = QtW.QSlider(QtCore.Qt.Orientation.Horizontal)
         self._slider_display_range = QtW.QLabel("?? - ??")
 
-        self._subtomogram_label = QtW.QLabel("")
-        self._layout.addWidget(self._subtomogram_label)
+        self._subtomo_label = QtW.QLabel("")
+        self._subtomo_paths: list[str] = []
+        self._layout.addWidget(self._subtomo_label)
         hlayout = QtW.QHBoxLayout()
         hlayout.setContentsMargins(0, 0, 0, 0)
         hlayout.addWidget(QtW.QLabel("Display Range:"))
@@ -80,13 +81,10 @@ class QExtractJobViewer(QJobScrollArea):
         tomo_names.sort(key=lambda x: x[0])
         self._tomo_list.set_choices(tomo_names)
         if _is_2d(self._job_dir):
-            self._subtomogram_label.setText("<b>Extracted 2D stacks (zero tilt)</b>")
-            self._subtomo_pattern = "{ith}_stack2d.mrcs"
+            _subtomo_label = "<b>Extracted 2D stacks (zero tilt)</b>"
         else:
-            self._subtomogram_label.setText(
-                "<b>Extracted subtomograms (max projection)</b>"
-            )
-            self._subtomo_pattern = "{ith}_data.mrc"
+            _subtomo_label = "<b>Extracted subtomograms (max projection)</b>"
+        self._subtomo_label.setText(_subtomo_label)
 
         if (
             (t0 := time.time()) - self._last_force_reloaded > 5.0
@@ -100,35 +98,34 @@ class QExtractJobViewer(QJobScrollArea):
         if value is None:
             return
         tomo_name = value[0]
-        max_num = _max_num_subtomograms(self._job_dir, tomo_name)
-        self._current_num_extracts = max_num
+        self._subtomo_paths = _list_subtomo_names(self._job_dir, tomo_name)
         current_pos = self._slider.value()
+        max_num = len(self._subtomo_paths)
         self._slider.setRange(0, max_num // self._num_page)
         current_pos = min(current_pos, self._slider.maximum())
         self._slider_value_changed(current_pos, udpate_slider=True)
 
+    def _end_index(self, start: int) -> int:
+        return min(start + self._num_page, len(self._subtomo_paths))
+
     def _slider_value_changed(self, value: int, *, udpate_slider: bool = False):
         # value: 0-indexed
-        self.window_closed_callback()
         if udpate_slider:
             self._slider.setValue(value)
-        self._text_edit.clear()
-        self._plot_session_id = self._text_edit.prep_uuid()
         start = value * self._num_page
         # Adjust for 1-based indexing
-        self._slider_display_range.setText(
-            f"{start + 1} - {min(start + self._num_page, self._current_num_extracts)}"
-        )
+        self._slider_display_range.setText(f"{start + 1} - {self._end_index(start)}")
+        self._plot_session_id = self._text_edit.prep_uuid()
+        self.window_closed_callback()
         self._worker = self.plot_extracts(start, self._plot_session_id)
         self._start_worker()
 
     @thread_worker
     def plot_extracts(self, start_index: int, session: uuid.UUID):
-        subtomo_dir = self._tomo_list.current_text(1)
-        end_index = min(start_index + self._num_page, self._current_num_extracts + 1)
-        subtomo_dir = self._job_dir.resolve_path(subtomo_dir)
-        for ith in range(start_index + 1, end_index + 1):
-            mrc_path = subtomo_dir / self._subtomo_pattern.format(ith=ith)
+        subtomo_dir = self._job_dir.resolve_path(self._tomo_list.current_text(1))
+        yield self._clear_text, session
+        for ith in range(start_index + 1, self._end_index(start_index) + 1):
+            mrc_path = subtomo_dir / self._subtomo_paths[ith - 1]
             if not mrc_path.exists():
                 img_str = self._text_edit.image_to_base64(
                     np.zeros((2, 2), dtype=np.float32), f"{ith}"
@@ -136,27 +133,27 @@ class QExtractJobViewer(QJobScrollArea):
                 yield self._on_string_ready, (img_str, session)
                 continue
             with mrcfile.mmap(mrc_path) as mrc:
-                if mrc.data.ndim != 3:
-                    # this may happen if the subtomogram is being written right now
-                    img_str = self._text_edit.image_to_base64(
-                        np.zeros((2, 2), dtype=np.float32), f"{ith}"
-                    )
-                    yield self._on_string_ready, (img_str, session)
-                    continue
                 if ith == start_index + 1:  # first
                     angst = mrc.voxel_size.x
                     size = mrc.header.nx
                     msg = f"Image size: {size} pix ({size * angst:.1f} A)"
                     yield self._on_text_ready, (msg + "\n\n", session)
-
-                if self._subtomo_pattern.endswith("_stack2d.mrcs"):
+                if mrc.data.ndim != 3:
+                    # this may happen if the subtomogram is being written right now
+                    img_2d = mrc.data
+                elif self._subtomo_paths[0].endswith("_stack2d.mrcs"):
                     img_2d = mrc.data[(mrc.data.shape[0] - 1) // 2, :, :]
                 else:
                     img_2d = np.max(np.asarray(mrc.data), axis=0)
                 img_2d = np.asarray(img_2d, dtype=np.float32)
                 img_str = self._text_edit.image_to_base64(img_2d, f"{ith}", 0.2)
                 yield self._on_string_ready, (img_str, session)
-        self._worker = None
+        if self._plot_session_id == session:
+            self._worker = None
+
+    def _clear_text(self, value: uuid.UUID):
+        if value == self._plot_session_id:
+            self._text_edit.clear()
 
     def _on_text_ready(self, value: tuple[str, uuid.UUID]):
         text, my_uuid = value
@@ -171,24 +168,13 @@ class QExtractJobViewer(QJobScrollArea):
         self._text_edit.insert_base64_image(img_str)
 
 
-def _max_num_subtomograms(job_dir: _job_dir.JobDirectory, tomoname: str) -> int:
-    """Return the number of subtomograms for a given tomogram name."""
+def _list_subtomo_names(job_dir: _job_dir.JobDirectory, tomoname: str) -> list[str]:
     tomo_dir = job_dir.path / "Subtomograms" / tomoname
     if _is_2d(job_dir):
         suffix = "_stack2d.mrcs"
     else:
         suffix = "_data.mrc"
-    ndigits = 1
-    while True:
-        question_marks = "?" * ndigits
-        if next(tomo_dir.glob(f"{question_marks}{suffix}"), None):
-            ndigits += 1
-        else:
-            break
-    if ndigits == 1:
-        return 0
-    path = next(tomo_dir.rglob(f"{'?' * (ndigits - 1)}{suffix}"))
-    return int(path.name[: -len(suffix)])
+    return [p for p in os.listdir(tomo_dir) if p.endswith(suffix)]
 
 
 def _is_2d(job_dir: _job_dir.JobDirectory) -> bool:
