f61ff0af78dac0f3bd1f3b1f1f487fa1504c735c
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2026-06-08 23:22:26 +0100

    remove assert

--- src/himena_relion/pipeline_watcher.py
+++ src/himena_relion/pipeline_watcher.py
@@ -192,7 +192,6 @@ def execute_job(
     cwd=None,
 ) -> RelionJobExecution:
     """Execute a RELION job named `job_name` (such as "Class3D/job012/")."""
-    assert "# version 50001" in Path("default_pipeline.star").read_text()
     job_name = normalize_job_id(job_name)
     try:
         job_dir = _job_dir.JobDirectory(Path(job_name).resolve())
